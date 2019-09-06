058daf3ea1043988607eaf44950aff0a7f523e76
davejang <djunkaroo621@users.noreply.github.com>
2019-09-05 23:03:03 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,1 +1,51 @@
 // Javascript for CALCULATOR APP
+document.getElementById("one").addEventListener("click", jam1);
+
+function jam1() {
+  document.getElementById("displayX").innerHTML= 1;
+}
+document.getElementById("two").addEventListener("click", jam2);
+
+function jam2() {
+  document.getElementById("displayX").innerHTML= 2;
+}
+document.getElementById("three").addEventListener("click", jam3);
+
+function jam3() {
+  document.getElementById("displayX").innerHTML= 3;
+}
+document.getElementById("four").addEventListener("click", jam4);
+
+function jam4() {
+  document.getElementById("displayX").innerHTML= 4;
+}
+document.getElementById("five").addEventListener("click", jam5);
+
+function jam5() {
+  document.getElementById("displayX").innerHTML= 5;
+}
+document.getElementById("six").addEventListener("click", jam6);
+
+function jam6() {
+  document.getElementById("displayX").innerHTML= 6;
+}
+document.getElementById("seven").addEventListener("click", jam7);
+
+function jam7() {
+  document.getElementById("displayX").innerHTML= 7;
+}
+document.getElementById("eight").addEventListener("click", jam8);
+
+function jam8() {
+  document.getElementById("displayX").innerHTML= 8;
+}
+document.getElementById("nine").addEventListener("click", jam9);
+
+function jam9() {
+  document.getElementById("displayX").innerHTML= 9;
+}
+document.getElementById("zero").addEventListener("click", jam0);
+
+function jam0() {
+  document.getElementById("displayX").innerHTML= 0;
+}
