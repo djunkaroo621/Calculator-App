02b1bf9b70aba55b9f85987454d4039a731c78eb
davejang <djunkaroo621@users.noreply.github.com>
2019-09-07 13:40:54 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,71 +1,102 @@
 // Javascript for CALCULATOR APP
+var numArray1= [];
+
+// numArray1.join();
+// console.log(numArray1.join([0,1,2]));
+
 document.getElementById("one").addEventListener("click", jam1);
 
 function jam1() {
-  document.getElementById("displayX").innerHTML= 1;
+  var num1= 1;
+  numArray1.push(num1);
+  console.log(numArray1);
 }
 document.getElementById("two").addEventListener("click", jam2);
 
 function jam2() {
-  document.getElementById("displayX").innerHTML= 2;
+  var num2= 2;
+  numArray1.push(num2);
+  console.log(numArray1);
 }
 document.getElementById("three").addEventListener("click", jam3);
 
 function jam3() {
-  document.getElementById("displayX").innerHTML= 3;
+  var num3= 3;
+  console.log(numArray1);
 }
 document.getElementById("four").addEventListener("click", jam4);
 
 function jam4() {
-  document.getElementById("displayX").innerHTML= 4;
+  var num4= 4;
+  console.log(numArray1);
 }
 document.getElementById("five").addEventListener("click", jam5);
 
 function jam5() {
-  document.getElementById("displayX").innerHTML= 5;
+  var num5= 5;
+  console.log(numArray1);
 }
 document.getElementById("six").addEventListener("click", jam6);
 
 function jam6() {
-  document.getElementById("displayX").innerHTML= 6;
+  var num6= 6;
+  console.log(numArray1);
 }
 document.getElementById("seven").addEventListener("click", jam7);
 
 function jam7() {
-  document.getElementById("displayX").innerHTML= 7;
+  var num7= 7;
+  console.log(numArray1);
 }
 document.getElementById("eight").addEventListener("click", jam8);
 
 function jam8() {
-  document.getElementById("displayX").innerHTML= 8;
+  var num8= 8;
+  console.log(numArray1);
 }
 document.getElementById("nine").addEventListener("click", jam9);
 
 function jam9() {
-  document.getElementById("displayX").innerHTML= 9;
+  var num9= 9;
+  console.log(numArray1);
 }
 document.getElementById("zero").addEventListener("click", jam0);
 
 function jam0() {
-  document.getElementById("displayX").innerHTML= 0;
+  var num0= 0;
+  console.log(numArray1);
 }
-document.getElementById("plus").addEventListener("click", jamPlus);
+document.getElementById("plus").addEventListener("click",jamPlus);
 
 function jamPlus() {
-  document.getElementById("displayX").innerHTML= "+";
+  var numPlus= "+";
+  console.log(numArray1);
 }
-document.getElementById("minus").addEventListener("click", jamMinus);
+var smushed= numArray1.join('');
+console.log(smushed);
 
-function jamMinus() {
-  document.getElementById("displayX").innerHTML= "-";
-}
-document.getElementById("multiply").addEventListener("click", jamMultiply);
 
-function jamMultiply() {
-  document.getElementById("displayX").innerHTML= "*";
-}
-document.getElementById("divide").addEventListener("click", jamDivide);
 
-function jamDivide() {
-  document.getElementById("displayX").innerHTML= "/";
-}
+
+// numArray1.join());
+
+// document.getElementById("plus").addEventListener("click", jamPlus);
+//
+// function jamPlus() {
+//   document.getElementById("displayX").innerHTML= "+";
+// }
+// document.getElementById("minus").addEventListener("click", jamMinus);
+//
+// function jamMinus() {
+//   document.getElementById("displayX").innerHTML= "-";
+// }
+// document.getElementById("multiply").addEventListener("click", jamMultiply);
+//
+// function jamMultiply() {
+//   document.getElementById("displayX").innerHTML= "*";
+// }
+// document.getElementById("divide").addEventListener("click", jamDivide);
+//
+// function jamDivide() {
+//   document.getElementById("displayX").innerHTML= "/";
+// }
