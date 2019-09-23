d54c06e66ed597565d1e174f87e3a08a9ac2d42c
davejang <djunkaroo621@users.noreply.github.com>
2019-09-22 21:03:57 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,91 +1,94 @@
 // Javascript for CALCULATOR APP
-var numArray1 = [];
+const numArray1 = [];
 
 const press1 = () => {
-  var num1 = 1;
+  var num1 = "1";
   const array = numArray1.push(num1);
-  const connectedNum = numArray1.join('')
+  const connectedNum = numArray1.join('');
   console.log(numArray1.join(''));
   document.getElementById("displayX").innerHTML = connectedNum;
 }
 document.getElementById("one").addEventListener("click", press1);
 
 const press2 = () => {
-  var num2 = 2;
+  var num2 = "2";
   const array = numArray1.push(num2);
-  const connectedNum = numArray1.join('')
+  const connectedNum = numArray1.join('');
+  numArray1.toString();
+
   console.log(numArray1.join(''));
   document.getElementById("displayX").innerHTML = connectedNum;
+
 }
 document.getElementById("two").addEventListener("click", press2);
 
 const press3 = () => {
-  var num3 = 3;
+  var num3 = "3";
   const array = numArray1.push(num3);
-  const connectedNum = numArray1.join('')
+  const connectedNum = numArray1.join('');
   console.log(numArray1.join(''));
   document.getElementById("displayX").innerHTML = connectedNum;
 }
 document.getElementById("three").addEventListener("click", press3);
 
 const press4 = () => {
-  var num4 = 4;
+  var num4 = "4";
   const array = numArray1.push(num4);
-  const connectedNum = numArray1.join('')
+  const connectedNum = numArray1.join('');
   console.log(numArray1.join(''));
   document.getElementById("displayX").innerHTML = connectedNum;
 }
 document.getElementById("four").addEventListener("click", press4);
 
 const press5 = () => {
-  var num5 = 5;
+  var num5 = "5";
   const array = numArray1.push(num5);
-  const connectedNum = numArray1.join('')
+  const connectedNum = numArray1.join('');
   console.log(numArray1.join(''));
   document.getElementById("displayX").innerHTML = connectedNum;
 }
 document.getElementById("five").addEventListener("click", press5);
 
 const press6 = () => {
-  var num6 = 6;
+  var num6 = "6";
   const array = numArray1.push(num6);
-  const connectedNum = numArray1.join('')
+  const connectedNum = numArray1.join('');
   console.log(numArray1.join(''));
   document.getElementById("displayX").innerHTML = connectedNum;
 }
 document.getElementById("six").addEventListener("click", press6);
 
 const press7 = () => {
-  var num7 = 7;
+  var num7 = "7";
   const array = numArray1.push(num7);
-  const connectedNum = numArray1.join('')
+  const connectedNum = numArray1.join('');
   console.log(numArray1.join(''));
   document.getElementById("displayX").innerHTML = connectedNum;
 }
 document.getElementById("seven").addEventListener("click", press7);
 
 const press8 = () => {
-  var num8 = 8;
+  var num8 = "8";
   const array = numArray1.push(num8);
-  const connectedNum = numArray1.join('')
+  const connectedNum = numArray1.join('');
   console.log(numArray1.join(''));
   document.getElementById("displayX").innerHTML = connectedNum;
 }
 document.getElementById("eight").addEventListener("click", press8);
 
 const press9 = () => {
-  var num9 = 9;
+  var num9 = "9";
   const array = numArray1.push(num9);
-  const connectedNum = numArray1.join('')
+  const connectedNum = numArray1.join('');
   console.log(numArray1.join(''));
   document.getElementById("displayX").innerHTML = connectedNum;
 }
 document.getElementById("nine").addEventListener("click", press9);
 
 const press0 = () => {
-  var num0 = 0;
+  var num0 = "0";
   const array = numArray1.push(num0);
-  const connectedNum = numArray1.join('')
+  const connectedNum = numArray1.join('');
   console.log(numArray1.join(''));
   document.getElementById("displayX").innerHTML = connectedNum;
 }
@@ -94,7 +97,7 @@ document.getElementById("zero").addEventListener("click", press0);
 const pressPlus = () => {
   var numPlus = "+";
   const array = numArray1.push(numPlus);
-  const connectedNum = numArray1.join('')
+  const connectedNum = numArray1.join('');
   console.log(numArray1.join(''));
   document.getElementById("displayX").innerHTML = connectedNum;
 }
@@ -103,7 +106,7 @@ document.getElementById("plus").addEventListener("click", pressPlus);
 const pressMinus = () => {
   var numMinus = "-";
   const array = numArray1.push(numMinus);
-  const connectedNum = numArray1.join('')
+  const connectedNum = numArray1.join('');
   console.log(numArray1.join(''));
   document.getElementById("displayX").innerHTML = connectedNum;
 }
@@ -112,7 +115,7 @@ document.getElementById("minus").addEventListener("click", pressMinus);
 const pressMultiply = () => {
   var numMultiply = "*";
   const array = numArray1.push(numMultiply);
-  const connectedNum = numArray1.join('')
+  const connectedNum = numArray1.join('');
   console.log(numArray1.join(''));
   document.getElementById("displayX").innerHTML = connectedNum;
 }
@@ -121,7 +124,7 @@ document.getElementById("multiply").addEventListener("click", pressMultiply);
 const pressDivide = () => {
   var numDivide = "/";
   const array = numArray1.push(numDivide);
-  const connectedNum = numArray1.join('')
+  const connectedNum = numArray1.join('');
   console.log(numArray1.join(''));
   document.getElementById("displayX").innerHTML = connectedNum;
 }
@@ -130,8 +133,21 @@ document.getElementById("divide").addEventListener("click", pressDivide);
 const pressDecimal = () => {
   var numDecimal = ".";
   const array = numArray1.push(numDecimal);
-  const connectedNum = numArray1.join('')
+  const connectedNum = numArray1.join('');
   console.log(numArray1.join(''));
   document.getElementById("displayX").innerHTML = connectedNum;
 }
 document.getElementById("decimal").addEventListener("click", pressDecimal);
+
+const pressEqual = () => {
+  const evalNumbers = numArray1.join('');
+  const result = eval(evalNumbers);
+  console.log(result);
+  document.getElementById("displayX").innerHTML = result;
+}
+document.getElementById("equal").addEventListener("click", pressEqual);
+
+const pressClear = (numArray1) => {
+    document.getElementById("displayX").innerHTML= "";
+}
+document.getElementById("clear").addEventListener("click", pressClear);
